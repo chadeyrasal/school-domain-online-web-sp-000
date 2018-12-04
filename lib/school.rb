b8bebd173cd92c9a182836d84e99585a2f5f52d1
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-04 17:03:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    roster = {}
+  end
+
+  def roster
+    roster
+  end
+
+  def add_student(student, grade)
+    roster[grade] = []
+    roster[grade] << student
+  end
+
+end
